42e62c20184005ce754acbcea77892a71afb7e24
Michael Maline <mmaline@unomaha.edu>
2022-01-28 16:29:40 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Toby Heinemann & Michael Maline
+ *         theinemann2@huskers.unl.edu
+ *         mmaline2@huskers.unl.edu
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Toby Heinemann and Michael Maline?\n");
+  printf("Electrical Engineering and Computer Engineering\n");
+
+  return 0;
+}
